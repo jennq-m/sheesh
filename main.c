9850057ea469e19ffce7d9506126b478fae4b707
Monocerum <monoceruscaelum@gmail.com>
2024-11-19 23:01:02 +0800

    feat: add struct for token

--- main.c
+++ main.c
@@ -6,8 +6,15 @@
 // Token types
 typedef enum {
     IDENTIFIER, KEYWORD, RESERVED_WORD, CONSTANT, NOISE_WORD, COMMENT,
-    OPERATOR, DELIMITER, INVALID
+    OPERATOR, DELIMITER, WHITESPACE, INVALID
 } TokenType;
+
+typedef struct {
+    TokenType type;
+    char *token_type;
+    char *value;
+    int line;
+} Token;
 
 // Keywords, Reserved Words, and Noise Words
 const char *keywords[] = {
@@ -25,7 +32,8 @@ const char *noiseWords[] = { "aylist", "eat", "put", "tier", "tra", "wise", NULL
 int isKeyword(const char *word);
 int isReservedWord(const char *word);
 int isNoiseWord(const char *word);
-TokenType fsmClassify(const char *token);
+Token newToken(const char *value, TokenType type, int line);
+Token fsmClassify(const char *token, int line);
 void analyzeLine(FILE *outputFile, char *line, int lineNumber);
 
 int main(int argc, char *argv[]) {
@@ -97,7 +105,74 @@ int isNoiseWord(const char *word) {
     return 0;
 }
 
-TokenType fsmClassify(const char *token) {
+char *typeToString(TokenType type) {
+     switch (type) {
+        case 0: return "Identifier";
+        case 1: return "Keyword";
+        case 2: return "Reserved Word";
+        case 3: return "Constant";
+        case 4: return "Noise Word";
+        case 5: return "Comment";
+        case 6: return "Operator";
+        case 7: return "Delimiter";
+        case 8: return "Whitespace";
+        case 9: return "Invalid";
+        default: return "Invalid";
+    }
+}
+
+int check_identifier(const char *token) {
+    int i = 0;
+    char current = token[i];
+    int state = 1;
+
+    while (current != '\0') {
+        switch(state) {
+            case 1: // Q1
+                if (isalpha(current) || current == '_' || current == '#') {
+                    state = 3;
+                } else {
+                    return 0;
+                }
+                break;
+            case 3: // Q3
+                if (isalnum(current)) {
+                    state = 3;
+                } else if (current == '#' || current == '_') {
+                    state = 4;
+                } else {
+                    return 0;
+                }
+                break;
+            case 4: // Q4
+                if (isalnum(current)) {
+                    state = 3;
+                } else if (current == '#' || current == '_') {
+                    state = 4;
+                } else {
+                    return 0;
+                }
+                break;
+            default:
+                return 0;
+        }
+
+        current = token[++i];
+    }
+
+    return state == 3;
+}
+
+Token newToken(const char *value, TokenType type, int line) {
+    Token token;
+    token.value = strdup(value);
+    token.type = type;
+    token.line = line;
+
+    return token;
+}
+
+Token fsmClassify(const char *token, int line) {
     int state = 0; // Initial state
     char ch;
     int decimalCount = 0; // To track the number of decimal points in the number
@@ -105,140 +180,129 @@ TokenType fsmClassify(const char *token) {
     for (int i = 0; (ch = token[i]) != '\0'; i++) {
         switch (state) {
             case 0: // Initial state
-                if (isalpha(ch) || ch == '_' || ch == '#') state = 1; // Identifier
-                else if (isdigit(ch)) state = 2;                      // Constant (number)
-                else if (ch == '\'' || ch == '"') state = 3;          // Constant (char/string)
-                else if (strchr("=+-*/%&|!<>", ch)) state = 4;        // Operator
-                else if (strchr(",;(){}[]", ch)) state = 5;          // Delimiter
-                else if (ch == '/' && token[i + 1] == '/') return COMMENT; // Single-line comment
-                else if (ch == '/' && token[i + 1] == '*') return COMMENT; // Multi-line comment
-                else return INVALID;
+                if (isalpha(ch) || ch == '_' || ch == '#') state = 1;
+                else if (isdigit(ch)) state = 2;
+                else if (ch == '\'' || ch == '"') state = 3;
+                else if (strchr("=+-*/%&|!<>", ch)) state = 4;
+                else if (strchr(",;(){}[]", ch)) state = 5;
+                else if (ch == '/' && token[i + 1] == '/') return newToken(token, COMMENT, line);
+                else if (ch == '/' && token[i + 1] == '*') return newToken(token, COMMENT, line);
+                else return newToken(token, INVALID, line);
                 break;
             case 1: // Identifier
-                if (!isalnum(ch) && ch != '_' && ch != '#') return INVALID;
+                if (!isalnum(ch) && ch != '_' && ch != '#') return newToken(token, INVALID, line);
                 break;
             case 2: // Constant (number)
                 if (ch == '.') {
-                    if (decimalCount > 0) return INVALID; // If there's already a decimal point, it's invalid
-                    decimalCount++; // Increment the decimal count
+                    if (decimalCount > 0) return newToken(token, INVALID, line);
+                    decimalCount++;
                 } else if (!isdigit(ch)) {
-                    return INVALID; // Any non-digit that is not a decimal is invalid
+                    return newToken(token, INVALID, line);
                 }
                 break;
             case 3: // Constant (char/string)
-                if (ch == '\'' || ch == '"') return CONSTANT;
+                if (ch == '\'' || ch == '"') return newToken(token, CONSTANT, line);
                 break;
             case 4: // Operator
-                if (!strchr("=+-*/%&|!<>", ch)) return INVALID;
+                if (!strchr("=+-*/%&|!<>", ch)) return newToken(token, INVALID, line);
                 break;
             case 5: // Delimiter
-                return DELIMITER;
+                return newToken(token, DELIMITER, line);
         }
     }
 
     // Final classification based on state
     if (state == 1) {
-        if (isKeyword(token)) return KEYWORD;
-        if (isReservedWord(token)) return RESERVED_WORD;
-        if (isNoiseWord(token)) return NOISE_WORD;
-        return IDENTIFIER;
+        if (isKeyword(token)) return newToken(token, KEYWORD, line);
+        if (isReservedWord(token)) return newToken(token, RESERVED_WORD, line);
+        if (isNoiseWord(token)) return newToken(token, NOISE_WORD, line);
+        if (check_identifier(token)) return newToken(token, IDENTIFIER, line);
+        return newToken(token, INVALID, line);
     } else if (state == 2 || state == 3) {
-        return CONSTANT;
+        return newToken(token, CONSTANT, line);
     } else if (state == 4) {
-        return OPERATOR;
+        return newToken(token, OPERATOR, line);
     } else if (state == 5) {
-        return DELIMITER;
+        return newToken(token, DELIMITER, line);
     }
-    return INVALID;
+
+    return newToken(token, INVALID, line);
 }
+
 
 void analyzeLine(FILE *outputFile, char *line, int lineNumber) {
     static int inMultiLineComment = 0; // Tracks whether inside a multi-line comment
-    char *token = strtok(line, " \t\n");
+    char *tokenStr = strtok(line, " \t\n");
     
-    while (token != NULL) {
+    while (tokenStr != NULL) {
         if (inMultiLineComment) {
             // Check if the multi-line comment ends in this token
-            char *endComment = strstr(token, "*/");
-            if (endComment) {
-                inMultiLineComment = 0; // Exit multi-line comment state
-                token = strtok(NULL, " \t\n"); // Continue with the rest of the line
-                continue;
-            }
+            char *endComment = strstr(tokenStr, "*/");
+
             // If still inside multi-line comment, skip token
-            token = strtok(NULL, " \t\n");
-            continue;
-        }
-        
-        // Check for single-line and multi-line comment starts
-        if (strncmp(token, "//", 2) == 0) {
-            fprintf(outputFile, "Line %d: %-15s Type: Comment\n", lineNumber, token);
-            break; // Ignore the rest of the line
-        } else if (strncmp(token, "/*", 2) == 0) {
-            fprintf(outputFile, "Line %d: %-15s Type: Comment\n", lineNumber, token);
-            // Check if the multi-line comment ends in this token
-            char *endComment = strstr(token, "*/");
             if (endComment) {
-                // Multi-line comment ends within the same token
-                token = strtok(NULL, " \t\n");
+                inMultiLineComment = 0;
+                Token token = newToken("*/", COMMENT, lineNumber);
+                fprintf(outputFile, "Line %d: Lexeme: %-15s Token: %s\n", token.line, token.value, typeToString(token.type));
+                free(token.value);
+                tokenStr = strtok(NULL, " \t\n");
                 continue;
             }
-            // Otherwise, set the multi-line comment state
-            inMultiLineComment = 1;
-            token = strtok(NULL, " \t\n");
+
+            tokenStr = strtok(NULL, " \t\n");
             continue;
         }
 
-        // Split the token further if it contains delimiters
+        if (strncmp(tokenStr, "//", 2) == 0) {
+            Token token = newToken("//", COMMENT, lineNumber);
+            fprintf(outputFile, "Line %d: Lexeme: %-15s Token: %s\n", token.line, token.value, typeToString(token.type));
+            free(token.value);
+            return;
+        }
+
+        if (strncmp(tokenStr, "/*", 2) == 0) {
+            Token token = newToken("/*", COMMENT, lineNumber);
+            fprintf(outputFile, "Line %d: Lexeme: %-15s Token: %s\n", token.line, token.value, typeToString(token.type));
+            free(token.value);
+
+            if (strstr(tokenStr, "*/")) {
+                tokenStr = strtok(NULL, " \t\n");
+                continue;
+            }
+
+            inMultiLineComment = 1;
+            tokenStr = strtok(NULL, " \t\n");
+            continue;
+        }
+
         char temp[256];
         int j = 0;
-        for (int i = 0; token[i] != '\0'; i++) {
-            if (strchr(",;(){}[]", token[i])) {
-                // Process the preceding substring as a separate token
+        for (int i = 0; tokenStr[i] != '\0'; i++) {
+            if (strchr(",;(){}[]", tokenStr[i])) {
                 if (j > 0) {
                     temp[j] = '\0';
-                    TokenType type = fsmClassify(temp);
-                    fprintf(outputFile, "Line %d: %-15s Type: ", lineNumber, temp);
-                    switch (type) {
-                        case KEYWORD: fprintf(outputFile, "Keyword\n"); break;
-                        case RESERVED_WORD: fprintf(outputFile, "Reserved Word\n"); break;
-                        case NOISE_WORD: fprintf(outputFile, "Noise Word\n"); break;
-                        case IDENTIFIER: fprintf(outputFile, "Identifier\n"); break;
-                        case CONSTANT: fprintf(outputFile, "Constant\n"); break;
-                        case OPERATOR: fprintf(outputFile, "Operator\n"); break;
-                        case DELIMITER: fprintf(outputFile, "Delimiter\n"); break;
-                        case COMMENT: fprintf(outputFile, "Comment\n"); break;
-                        default: fprintf(outputFile, "Invalid\n"); break;
-                    }
-                    j = 0; // Reset temp buffer
+                    Token token = fsmClassify(temp, lineNumber);
+                    fprintf(outputFile, "Line %d: Lexeme: %-15s \tToken: %s\n", token.line, token.value, typeToString(token.type));
+                    free(token.value);
+                    j = 0;
                 }
 
-                // Process the delimiter itself
-                char delim[2] = {token[i], '\0'};
-                fprintf(outputFile, "Line %d: %-15s Type: Delimiter\n", lineNumber, delim);
+                char delim[2] = {tokenStr[i], '\0'};
+                Token token = newToken(delim, DELIMITER, lineNumber);
+                fprintf(outputFile, "Line %d: Lexeme: %-15s Token: Delimiter\n", token.line, token.value);
+                free(token.value);
             } else {
-                temp[j++] = token[i];
+                temp[j++] = tokenStr[i];
             }
         }
 
-        // Process any remaining characters as a token
         if (j > 0) {
             temp[j] = '\0';
-            TokenType type = fsmClassify(temp);
-            fprintf(outputFile, "Line %d: %-15s Type: ", lineNumber, temp);
-            switch (type) {
-                case KEYWORD: fprintf(outputFile, "Keyword\n"); break;
-                case RESERVED_WORD: fprintf(outputFile, "Reserved Word\n"); break;
-                case NOISE_WORD: fprintf(outputFile, "Noise Word\n"); break;
-                case IDENTIFIER: fprintf(outputFile, "Identifier\n"); break;
-                case CONSTANT: fprintf(outputFile, "Constant\n"); break;
-                case OPERATOR: fprintf(outputFile, "Operator\n"); break;
-                case DELIMITER: fprintf(outputFile, "Delimiter\n"); break;
-                case COMMENT: fprintf(outputFile, "Comment\n"); break;
-                default: fprintf(outputFile, "Invalid\n"); break;
-            }
+            Token token = fsmClassify(temp, lineNumber);
+            fprintf(outputFile, "Line %d: Lexeme: %-15s Token: %s\n", token.line, token.value, typeToString(token.type));
+            free(token.value);
         }
 
-        token = strtok(NULL, " \t\n");
+        tokenStr = strtok(NULL, " \t\n");
     }
 }
