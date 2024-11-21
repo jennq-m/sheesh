4db017b5e9ac9f889b5c2a7e83ed9249940680a1
Geru <113689254+Gelatino0308@users.noreply.github.com>
2024-11-21 10:54:34 +0800

    feat: include header file; fix: single quote with string invalidation

---
+++ lex.h
@@ -0,0 +1,116 @@
+#ifndef LEX_H
+#define LEX_H
+
+#include <stdio.h>
+
+// Enumeration for token types
+typedef enum {
+    IDENTIFIER, 
+    KEYWORD, 
+    RESERVED_WORD, 
+    CONSTANT, 
+    NOISE_WORD, 
+    COMMENT, 
+    ARITHMETIC_OPE, 
+    ASSIGNMENT_OPE, 
+    LOGICAL_OPE, 
+    UNARY_OPE, 
+    RELATIONAL_OPE, 
+    COMMA, 
+    SEMICOLON, 
+    QUOTATION, 
+    O_PARENTHESIS, 
+    C_PARENTHESIS, 
+    O_BRACE, 
+    C_BRACE, 
+    O_BRACKET, 
+    C_BRACKET, 
+    INVALID, 
+    INT_CONSTANT, 
+    FLOAT_CONSTANT, 
+    STRING_CONSTANT, 
+    CONSTANDRESERVED, 
+    CHAR_CONSTANT
+} TokenType;
+
+// Structure to represent a token
+typedef struct {
+    char *value;       
+    TokenType type;    
+    int sheeshLine;    
+} Token;
+
+
+Token newToken(const char *value, TokenType type, int sheeshLine);
+Token sheeshLexer(const char *sheeshLexeme, int sheeshLine);
+void analyzeLine(FILE *outputSheesh, char *sheeshLine, int sheeshLineNum);
+
+// Function prototypes
+int checkFilename(int argc, char *argv);
+int checkKeyword(const char *sheeshLexeme);
+int checkReservedWord(const char *sheeshLexeme);
+int checkNoiseWord(const char *sheeshLexeme);
+int checkAssignment(const char *sheeshLexeme);
+int checkLogical(const char *sheeshLexeme);
+int checkArithmetic(const char *sheeshLexeme);
+int checkUnary(const char *sheeshLexeme);
+int checkRelational(const char *sheeshLexeme);
+
+char *typeToString(TokenType type) {
+    switch (type) {
+        case IDENTIFIER: 
+            return "Identifier";
+        case KEYWORD: 
+            return "Keyword";
+        case RESERVED_WORD: 
+            return "Reserved Word";
+        case CONSTANT: 
+            return "Constant";
+        case CONSTANDRESERVED: 
+            return "Constant (Legit (bool in C)), Reserved Word";
+        case NOISE_WORD: 
+            return "Noise Word";
+        case COMMENT: 
+            return "Comment";
+        case ARITHMETIC_OPE: 
+            return "Arithmetic Operator";
+        case ASSIGNMENT_OPE: 
+            return "Assignment Operator";
+        case LOGICAL_OPE: 
+            return "Logical Operator (Boolean)";
+        case UNARY_OPE: 
+            return "Unary Operator";
+        case RELATIONAL_OPE: 
+            return "Relational Operator (Boolean)";
+        case COMMA:
+            return "Delimiter (Comma)";
+        case SEMICOLON:
+            return "Delimiter (Semicolon)";
+        case O_PARENTHESIS:
+            return "Delimiter (Open (Left) Parenthesis)";
+        case C_PARENTHESIS:
+            return "Delimiter (Closing (Right) Parenthesis)";
+        case O_BRACKET:
+            return "Bracket (Open (Left) Bracket)";
+        case C_BRACKET:
+            return "Bracket (Closing (Right) Bracket)";
+        case O_BRACE:
+            return "Bracket (Open (Left) Brace)";
+        case C_BRACE:
+            return "Bracket (Closing (Right) Brace)";
+        case INT_CONSTANT: 
+            return "Constant (Num (int in C))";
+        case FLOAT_CONSTANT: 
+            return "Constant (Drift (float in C))";
+        case STRING_CONSTANT: 
+            return "Constant (Text (string in C))";
+        case CHAR_CONSTANT:
+            return "Constant (Vibe (char in C))";
+        case INVALID: 
+            return "Invalid";
+        default: 
+            return "Invalid";
+    }
+}
+
+#endif
--- main.c
+++ main.c
@@ -3,38 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
-//Enumariting different token type
-typedef enum {
-    IDENTIFIER, KEYWORD, RESERVED_WORD, CONSTANT, NOISE_WORD, COMMENT, 
-    ARITHMETIC_OPE, ASSIGNMENT_OPE, LOGICAL_OPE, UNARY_OPE, RELATIONAL_OPE, 
-    COMMA, SEMICOLON, QUOTATION, O_PARENTHESIS, C_PARENTHESIS, O_BRACE, 
-    C_BRACE, O_BRACKET, C_BRACKET, INVALID, INT_CONSTANT, 
-    FLOAT_CONSTANT, STRING_CONSTANT, CONSTANDRESERVED, CHAR_CONSTANT
-} TokenType;
+#include "lex.h"
 
-//Holding attributes of TOKEN
-typedef struct {
-    TokenType type;
-    char *token_type;
-    char *value;
-    int sheeshLine;
-} Token;
-
-const char *noiseWords[] = { "aylist", "eat", "put", "tier", "tra", "wise", NULL };
-
-//Declaration of Function Prototype
-int checkFilename(int argc, char *argv);
-int checkKeyword(const char *sheeshLexeme);
-int checkReservedWord(const char *sheeshLexeme);
-int checkNoiseWord(const char *sheeshLexeme);
-int checkAssignment(const char *sheeshLexeme);
-int checkLogical(const char *sheeshLexeme);
-int checkArithmetic(const char *sheeshLexeme);
-int checkUnary(const char *sheeshLexeme);
-int checkRelational(const char *sheeshLexeme);
-Token newToken(const char *value, TokenType type, int sheeshLine);
-Token sheeshLexer(const char *sheeshLexeme, int sheeshLine);
-void analyzeLine(FILE *outputSheesh, char *sheeshLine, int sheeshColumn);
 
 //Function where reading .shs file and writing to the SymbolTable.txt
 int main(int argc, char *argv[]) {
@@ -362,6 +332,8 @@ int checkReservedWord(const char *sheeshLexeme) {
 
 
 int checkNoiseWord(const char *sheeshLexeme) {
+    const char *noiseWords[] = { "aylist", "eat", "put", "tier", "tra", "wise", NULL };
+
     for (int i = 0; noiseWords[i] != NULL; i++) {
         if (strcmp(sheeshLexeme, noiseWords[i]) == 0) {
             return 1;
@@ -474,71 +446,13 @@ int checkUnary(const char *sheeshLexeme) {
 int checkRelational(const char *sheeshLexeme) {
     char *relational_operators[6] = {"==", "!=", ">", "<", ">=", "<="};
 
-    for (int i = 0; i < sizeof(relational_operators) / sizeof(relational_operators[0]); i++) {
+    for (int i = 0; i < 6; i++) {
         if (strcmp(sheeshLexeme, relational_operators[i]) == 0) {
             return 1;
         }
     }
     
     return 0;
-}
-
-//Returning the corresponding token typ
-char *typeToString(TokenType type) {
-    switch (type) {
-        case IDENTIFIER: 
-            return "Identifier";
-        case KEYWORD: 
-            return "Keyword";
-        case RESERVED_WORD: 
-            return "Reserved Word";
-        case CONSTANT: 
-            return "Constant";
-        case CONSTANDRESERVED: 
-            return "Constant (Legit (bool in C)), Reserved Word";
-        case NOISE_WORD: 
-            return "Noise Word";
-        case COMMENT: 
-            return "Comment";
-        case ARITHMETIC_OPE: 
-            return "Arithmetic Operator";
-        case ASSIGNMENT_OPE: 
-            return "Assignment Operator";
-        case LOGICAL_OPE: 
-            return "Logical Operator (Boolean)";
-        case UNARY_OPE: 
-            return "Unary Operator";
-        case RELATIONAL_OPE: 
-            return "Relational Operator (Boolean)";
-        case COMMA:
-            return "Delimiter (Comma)";
-        case SEMICOLON:
-            return "Delimiter (Semicolon)";
-        case O_PARENTHESIS:
-            return "Delimiter (Open (Left) Parenthesis)";
-        case C_PARENTHESIS:
-            return "Delimiter (Closing (Right) Parenthesis)";
-        case O_BRACKET:
-            return "Bracket (Open (Left) Bracket)";
-        case C_BRACKET:
-            return "Bracket (Closing (Right) Bracket)";
-        case O_BRACE:
-            return "Bracket (Open (Left) Brace)";
-        case C_BRACE:
-            return "Bracket (Closing (Right) Brace)";
-        case INT_CONSTANT: 
-            return "Constant (Num (int in C))";
-        case FLOAT_CONSTANT: 
-            return "Constant (Drift (float in C))";
-        case STRING_CONSTANT: 
-            return "Constant (Text (string in C))";
-        case CHAR_CONSTANT:
-            return "Constant (Vibe (char in C))";
-        case INVALID: 
-            return "Invalid";
-        default: 
-            return "Invalid";
-    }
 }
 
 //Creating  and returning a new token 
@@ -551,7 +465,7 @@ Token newToken(const char *value, TokenType type, int sheeshColumn) {
     return token;
 }
 
-//
+//LEXER
 Token sheeshLexer(const char *sheeshLexeme, int sheeshLine) {
     int state = 0;
     char ch;
@@ -694,6 +608,7 @@ void analyzeLine(FILE *outputSheesh, char *sheeshLine, int sheeshColumn) {
     int characterConstant = 0;
     char temp[256];
     int tempMarker = 0;
+    int charDetected = 0;
 
     for (int i = 0; sheeshLine[i] != '\0'; i++) {
 
@@ -723,13 +638,35 @@ void analyzeLine(FILE *outputSheesh, char *sheeshLine, int sheeshColumn) {
 
         if (characterConstant) {
             temp[tempMarker++] = sheeshLine[i];
+
             if (sheeshLine[i] == '\'') {
-                temp[tempMarker] = '\0';
-                Token token = newToken(temp, CHAR_CONSTANT, sheeshColumn);
-                fprintf(outputSheesh, "Line %d: Lexeme: %-15s Token: %s\n", token.sheeshLine, token.value, typeToString(token.type));
-                free(token.value);
-                tempMarker = 0;
-                characterConstant = 0;
+                if (tempMarker == 3) {
+                    temp[tempMarker] = '\0';
+                    Token token = newToken(temp, CHAR_CONSTANT, sheeshColumn);
+                    fprintf(outputSheesh, "Line %d: Lexeme: %-15s Token: %s\n", token.sheeshLine, token.value, typeToString(token.type));
+                    free(token.value);
+                    tempMarker = 0;
+                    characterConstant = 0;
+                }
+                else {
+                    temp[tempMarker] = '\0';
+                    if (tempMarker <= 2) {
+                        Token token = newToken(temp, QUOTATION, sheeshColumn);
+                        fprintf(outputSheesh, "Line %d: Lexeme: %-15s Token: %s\n", token.sheeshLine, token.value, typeToString(token.type));
+                        free(token.value);
+                    }
+                    else {
+                        Token token = newToken(temp, INVALID, sheeshColumn);
+                        fprintf(outputSheesh, "Line %d: Lexeme: %-15s Token: %s\n", token.sheeshLine, token.value, typeToString(token.type));
+                        free(token.value);
+                    }
+
+                    tempMarker = 0;
+                    characterConstant = 0;
+                }
+            }
+            else {
+                charDetected += 1;
             }
             continue;
         }
@@ -956,16 +893,15 @@ void analyzeLine(FILE *outputSheesh, char *sheeshLine, int sheeshColumn) {
             Token token = sheeshLexer(temp, sheeshColumn);  
             fprintf(outputSheesh, "Line %d: Lexeme: %-15s Token: %s\n", token.sheeshLine, token.value, typeToString(token.type));
             free(token.value);  
+
         } else if (isspace(sheeshLine[i])) {
-           
             if (tempMarker > 0) {
                 temp[tempMarker] = '\0'; 
                 Token token = sheeshLexer(temp, sheeshColumn); 
                 if (token.type == CONSTANT) {
                     fprintf(outputSheesh, "Line %d: Lexeme: %-15s Token: %s\n", token.sheeshLine, token.value, typeToString(token.type));
-                } else {
-                    fprintf(outputSheesh, "Line %d: Lexeme: %-15s Token: %s\n", token.sheeshLine, token.value, typeToString(token.type));
                 }
+
                 free(token.value);  
                 tempMarker = 0;  
             }
