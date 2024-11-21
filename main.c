0a729b56ce17a068f7136a45bdeae06f8f61ecda
Geru <113689254+Gelatino0308@users.noreply.github.com>
2024-11-21 11:39:00 +0800

    feat: include lex.h

--- main.c
+++ main.c
@@ -3,38 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-//Enumariting different token type
-typedef enum {
-    IDENTIFIER, KEYWORD, RESERVED_WORD, CONSTANT, NOISE_WORD, COMMENT, 
-    ARITHMETIC_OPE, ASSIGNMENT_OPE, LOGICAL_OPE, UNARY_OPE, RELATIONAL_OPE, 
-    COMMA, SEMICOLON, QUOTATION, O_PARENTHESIS, C_PARENTHESIS, O_BRACE, 
-    C_BRACE, O_BRACKET, C_BRACKET, INVALID, INT_CONSTANT, 
-    FLOAT_CONSTANT, STRING_CONSTANT, CONSTANDRESERVED, CHAR_CONSTANT
-} TokenType;
-
-//Holding attributes of TOKEN
-typedef struct {
-    TokenType type;
-    char *token_type;
-    char *value;
-    int sheeshLine;
-} Token;
-
-const char *noiseWords[] = { "aylist", "eat", "put", "tier", "tra", "wise", NULL };
-
-//Declaration of Function Prototype
-int checkFilename(int argc, char *argv);
-int checkKeyword(const char *sheeshLexeme);
-int checkReservedWord(const char *sheeshLexeme);
-int checkNoiseWord(const char *sheeshLexeme);
-int checkAssignment(const char *sheeshLexeme);
-int checkLogical(const char *sheeshLexeme);
-int checkArithmetic(const char *sheeshLexeme);
-int checkUnary(const char *sheeshLexeme);
-int checkRelational(const char *sheeshLexeme);
-Token newToken(const char *value, TokenType type, int sheeshLine);
-Token sheeshLexer(const char *sheeshLexeme, int sheeshLine);
-void analyzeLine(FILE *outputSheesh, char *sheeshLine, int sheeshColumn);
+#include "lex.h"
 
 //Function where reading .shs file and writing to the SymbolTable.txt
 int main(int argc, char *argv[]) {
@@ -362,6 +331,7 @@ int checkReservedWord(const char *sheeshLexeme) {
 
 
 int checkNoiseWord(const char *sheeshLexeme) {
+    const char *noiseWords[] = { "aylist", "eat", "put", "tier", "tra", "wise", NULL };
     for (int i = 0; noiseWords[i] != NULL; i++) {
         if (strcmp(sheeshLexeme, noiseWords[i]) == 0) {
             return 1;
@@ -481,64 +451,6 @@ int checkRelational(const char *sheeshLexeme) {
     }
     
     return 0;
-}
-
-//Returning the corresponding token typ
-char *typeToString(TokenType type) {
-    switch (type) {
-        case IDENTIFIER: 
-            return "Identifier";
-        case KEYWORD: 
-            return "Keyword";
-        case RESERVED_WORD: 
-            return "Reserved Word";
-        case CONSTANT: 
-            return "Constant";
-        case CONSTANDRESERVED: 
-            return "Constant (Legit (bool in C)), Reserved Word";
-        case NOISE_WORD: 
-            return "Noise Word";
-        case COMMENT: 
-            return "Comment";
-        case ARITHMETIC_OPE: 
-            return "Arithmetic Operator";
-        case ASSIGNMENT_OPE: 
-            return "Assignment Operator";
-        case LOGICAL_OPE: 
-            return "Logical Operator (Boolean)";
-        case UNARY_OPE: 
-            return "Unary Operator";
-        case RELATIONAL_OPE: 
-            return "Relational Operator (Boolean)";
-        case COMMA:
-            return "Delimiter (Comma)";
-        case SEMICOLON:
-            return "Delimiter (Semicolon)";
-        case O_PARENTHESIS:
-            return "Delimiter (Open (Left) Parenthesis)";
-        case C_PARENTHESIS:
-            return "Delimiter (Closing (Right) Parenthesis)";
-        case O_BRACKET:
-            return "Bracket (Open (Left) Bracket)";
-        case C_BRACKET:
-            return "Bracket (Closing (Right) Bracket)";
-        case O_BRACE:
-            return "Bracket (Open (Left) Brace)";
-        case C_BRACE:
-            return "Bracket (Closing (Right) Brace)";
-        case INT_CONSTANT: 
-            return "Constant (Num (int in C))";
-        case FLOAT_CONSTANT: 
-            return "Constant (Drift (float in C))";
-        case STRING_CONSTANT: 
-            return "Constant (Text (string in C))";
-        case CHAR_CONSTANT:
-            return "Constant (Vibe (char in C))";
-        case INVALID: 
-            return "Invalid";
-        default: 
-            return "Invalid";
-    }
 }
 
 //Creating  and returning a new token 
