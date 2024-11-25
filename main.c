31ff8c1813e4bde8ba3f03ae3aca4f7d188b28b1
Geru <113689254+Gelatino0308@users.noreply.github.com>
2024-11-25 20:45:00 +0800

    feat: add the rest of keyword fsm case filter

--- main.c
+++ main.c
@@ -57,6 +57,7 @@ int checkKeyword(const char *sheeshLexeme) {
     int currChar = 0;
 
     switch (sheeshLexeme[currChar++]) {
+
         case 'b':
             switch(sheeshLexeme[currChar++]) {
                 case 'o':
@@ -69,17 +70,409 @@ int checkKeyword(const char *sheeshLexeme) {
                                             switch(sheeshLexeme[currChar++]) {
                                                 case 'e':
                                                     return 1;
-                                                break;
-                                            }
-                                        break;   
-                                    }
-                                break;     
-                            }
-                        break;    
+                                            }   
+                                    }     
+                            }   
                     } 
-                break; 
             }
-        break;
+
+            currChar = 0;
+
+        case 'c':
+            switch(sheeshLexeme[currChar++]) {
+                case 'a':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'r':
+                            return 1;
+                    }
+            }
+
+            currChar = 0;
+
+        case 'd':
+            switch(sheeshLexeme[currChar++]) {
+                case 'o':
+                    return 1;
+                
+                case 'r':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'i':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'f':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 't':
+                                            return 1;
+                                    }
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'e':
+            switch(sheeshLexeme[currChar++]) {
+                case 'm':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'p':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 't':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'y':
+                                            return 1;
+                                    }
+                            }
+                    }
+                
+                case 'x':
+                    return 1;
+            }
+
+            currChar = 0;
+
+        case 'f':
+            switch(sheeshLexeme[currChar++]) {
+                case 'l':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'i':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'p':
+                                    return 1;
+                                    
+                            }
+                    }
+                
+                case 'r':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'o':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'z':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'e':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 'n':
+                                                    return 1;
+                                            }
+                                    }
+                            }
+                    }
+            }
+
+            currChar = 0;
+        
+        case 'g':
+            switch(sheeshLexeme[currChar++]) {
+                case 'r':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'o':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'u':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'p':
+                                            return 1;
+                                    }   
+                                        
+                            }   
+                    } 
+            }
+
+            currChar = 0;
+
+        case 'i':
+            switch(sheeshLexeme[currChar++]) {
+                case 'f':
+                    return 1;
+                
+                case 'n':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'p':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'u':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 't':
+                                            return 1;
+                                    }
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'j':
+            switch(sheeshLexeme[currChar++]) {
+                case 'u':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'm':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'p':
+                                    return 1;    
+                            }   
+                    } 
+            }
+
+            currChar = 0;
+
+        case 'l':
+            switch(sheeshLexeme[currChar++]) {
+                case 'e':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'g':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'i':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 't':
+                                            return 1;
+                                    }
+                            }
+                    }
+                case 'o':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'c':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'k':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'e':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 'd':
+                                                    return 1;
+                                            }
+                                        case 'i':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 'n':
+                                                    return 1;
+                                            }
+                                    }
+                            }
+                        case 'n':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'g':
+                                    return 1;
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'm':
+            switch(sheeshLexeme[currChar++]) {
+                case 'e':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'a':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'n':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'w':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 'h':
+                                                    switch(sheeshLexeme[currChar++]) {
+                                                        case 'i':
+                                                            switch(sheeshLexeme[currChar++]) {
+                                                                case 'l':
+                                                                    switch(sheeshLexeme[currChar++]) {
+                                                                        case 'e':
+                                                                            return 1;
+                                                                    }
+                                                            }
+                                                    }
+                                            }
+                                    }
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'n':
+            switch(sheeshLexeme[currChar++]) {
+                case 'i':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'c':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'k':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'n':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 'a':
+                                                    switch(sheeshLexeme[currChar++]) {
+                                                        case 'm':
+                                                            switch(sheeshLexeme[currChar++]) {
+                                                                case 'e':
+                                                                    return 1;
+                                                            }
+                                                    }
+                                            }
+                                    }
+                            }
+                    }
+                case 'u':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'm':
+                            return 1;
+                    }
+            }
+
+            currChar = 0;
+
+        case 'o':
+            switch(sheeshLexeme[currChar++]) {
+                case 'p':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'e':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'n':
+                                    return 1;
+                            }
+                    }
+                case 't':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'h':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'e':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'r':
+                                            return 1;
+                                    }
+                            }
+                        case 'u':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 't':
+                                    return 1;
+                            }
+                    }
+                case 'u':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 't':
+                            switch(sheeshLexeme[currChar++]) {
+                                case '\0':
+                                    return 1;
+
+                                case 's':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'i':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 'd':
+                                                    switch(sheeshLexeme[currChar++]) {
+                                                        case 'e':
+                                                            return 1;
+                                                    }
+                                            }
+                                    }
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'p':
+            switch(sheeshLexeme[currChar++]) {
+                case 'l':
+                    return 1;
+            }
+
+            currChar = 0;
+
+        case 'r':
+            switch(sheeshLexeme[currChar++]) {
+                case 'e':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'p':
+                            return 1;
+                    }
+            }
+
+            currChar = 0;
+
+        case 's':
+            switch(sheeshLexeme[currChar++]) {
+                case 'c':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'e':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'n':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'a':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 'r':
+                                                    switch(sheeshLexeme[currChar++]) {
+                                                        case 'i':
+                                                            switch(sheeshLexeme[currChar++]) {
+                                                                case 'o':
+                                                                    return 1;
+                                                            }
+                                                    }
+                                            }
+                                    }
+                            }
+                    }
+                case 'h':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'o':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'r':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 't':
+                                            return 1;
+                                    }
+                            }
+                    }
+                case 't':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'a':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'n':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 'd':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 'a':
+                                                    switch(sheeshLexeme[currChar++]) {
+                                                        case 'r':
+                                                            switch(sheeshLexeme[currChar++]) {
+                                                                case 'd':
+                                                                    return 1;
+                                                            }
+                                                    }
+                                            }
+                                    }
+                            }
+                        case 'o':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'p':
+                                    return 1;
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 't':
+            switch(sheeshLexeme[currChar++]) {
+                case 'e':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'a':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'm':
+                                    return 1;
+                            }
+                        case 'x':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 't':
+                                    return 1;
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'v':
+            switch(sheeshLexeme[currChar++]) {
+                case 'i':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'b':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'e':
+                                    return 1;
+                            }
+                    }
+            }
     }
 
     return 0;
