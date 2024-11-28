e0dba0cfcb633072a0bbab2521c9715cd5fc9f1e
Geru <113689254+Gelatino0308@users.noreply.github.com>
2024-11-28 18:33:46 +0800

    feat: add noise words fsm case filter

--- main.c
+++ main.c
@@ -760,12 +760,102 @@ int checkReservedWord(const char *sheeshLexeme) {
 
 
 int checkNoiseWord(const char *sheeshLexeme) {
-    const char *noiseWords[] = { "aylist", "eat", "put", "tier", "tra", "wise", NULL };
-    for (int i = 0; noiseWords[i] != NULL; i++) {
-        if (strcmp(sheeshLexeme, noiseWords[i]) == 0) {
-            return 1;
-        }
+    int currChar = 0;
+
+    switch(sheeshLexeme[currChar++]) {
+        case 'a':
+            switch(sheeshLexeme[currChar++]) {
+                case 'y':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'l':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'i':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case 's':
+                                            switch(sheeshLexeme[currChar++]) {
+                                                case 't':
+                                                    switch(sheeshLexeme[currChar++]) {
+                                                        case '\0':
+                                                            return 1;
+                                                    }
+                                            }
+                                    }
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'e':
+            switch(sheeshLexeme[currChar++]) {
+                case 'a':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 't':
+                            switch(sheeshLexeme[currChar++]) {
+                                case '\0':
+                                    return 1;
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'p':
+            switch(sheeshLexeme[currChar++]) {
+                case 'u':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 't':
+                            switch(sheeshLexeme[currChar++]) {
+                                case '\0':
+                                    return 1;
+                            }
+                    }
+            }
+            
+            currChar = 0;
+
+        case 't':
+            switch(sheeshLexeme[currChar++]) {
+                case 'i':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'e':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'r':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case '\0':
+                                            return 1;
+                                    }
+                            }
+                    }
+                
+                case 'r':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 'a':
+                            switch(sheeshLexeme[currChar++]) {
+                                case '\0':
+                                    return 1;
+                            }
+                    }
+            }
+
+            currChar = 0;
+
+        case 'w':
+            switch(sheeshLexeme[currChar++]) {
+                case 'i':
+                    switch(sheeshLexeme[currChar++]) {
+                        case 's':
+                            switch(sheeshLexeme[currChar++]) {
+                                case 'e':
+                                    switch(sheeshLexeme[currChar++]) {
+                                        case '\0':
+                                            return 1;
+                                    }
+                            }
+                    }
+            }
     }
+
     return 0;
 }
 
