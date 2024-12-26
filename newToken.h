79b7d2d93ab8e3f2fc7520c2a258af2b4b378e2b
Geru <113689254+Gelatino0308@users.noreply.github.com>
2024-12-26 14:31:34 +0800

    chore: delete newToken.h file

--- newToken.h
+++
@@ -1,9 +0,0 @@
-//Creating and returning a new token 
-Token newToken(const char *value, TokenType type, int sheeshColumn) {
-    Token token;
-    token.value = strdup(value);
-    token.type = type;
-    token.sheeshLine = sheeshColumn;
-
-    return token;
-}
